d91ac7cd8c0b3afdc513d255e0f9b01a866836d6
Duy Tai <0985203883tai@gmail.com>
2024-06-19 00:57:32 +0700

    login, register xong

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,12 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        dataBinding = true;
+        viewBinding = true;
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,6 +43,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
